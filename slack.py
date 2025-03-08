bbcc409cd84361045f2fc37dadc4c6158cb5e361
keumky2 <keumky2@woowahan.com>
2025-03-08 19:40:28 +0900

    feat(slack): 블록 메시지 전송 기능 추가

--- slack.py
+++ slack.py
@@ -8,3 +8,12 @@ def post_message(token, channel, text):
         data={"channel": channel, "text": text},
     )
     print(response)
+
+
+def post_block_message(token, channel, blocks):
+    response = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer " + token},
+        json={"channel": channel, "blocks": blocks},
+    )
+    print(response)
