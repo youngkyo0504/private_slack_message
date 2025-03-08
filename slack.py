3e7a0d9b1530704f41aa357989dc9e8f54607562
keumky2 <keumky2@woowahan.com>
2025-03-08 19:33:01 +0900

    style(slack): 코드 포맷팅 개선

--- slack.py
+++ slack.py
@@ -1,8 +1,10 @@
 import requests
 
+
 def post_message(token, channel, text):
-    response = requests.post("https://slack.com/api/chat.postMessage",
-                             headers={"Authorization": "Bearer " + token},
-                             data={"channel": channel, "text": text}
-                             )
+    response = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer " + token},
+        data={"channel": channel, "text": text},
+    )
     print(response)
